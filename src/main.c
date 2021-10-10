4850dd5b123daff3aa8e7373076ebad04641423a
dcmde <43622430+dcmde@users.noreply.github.com>
2021-10-10 15:09:56 +0200

    Working example open loop six step.

--- src/main.c
+++ src/main.c
@@ -2,12 +2,13 @@
 #include <stm32f10x_rcc.h>
 #include <stm32f10x_gpio.h>
 #include <stm32f10x_tim.h>
+#include "misc.h"
 
 int main() {
     GPIO_InitTypeDef gpioInitTypeDef;
     TIM_TimeBaseInitTypeDef timInitStruct;
     TIM_BDTRInitTypeDef TIM_BDTRInitStructure;
-
+    NVIC_InitTypeDef nvicInitTypeDef;
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1 |
                            RCC_APB2Periph_AFIO |
@@ -15,6 +16,8 @@ int main() {
                            RCC_APB2Periph_GPIOA |
                            RCC_APB2Periph_GPIOB,
                            ENABLE);
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 
     gpioInitTypeDef.GPIO_Mode = GPIO_Mode_Out_PP;
     gpioInitTypeDef.GPIO_Pin = GPIO_Pin_13;
@@ -29,35 +32,56 @@ int main() {
     GPIO_Init(GPIOB, &gpioInitTypeDef);
 
     timInitStruct.TIM_RepetitionCounter = 0;
-    timInitStruct.TIM_Prescaler = 720;
-    timInitStruct.TIM_Period = 100;
+    timInitStruct.TIM_Prescaler = 36;
+    timInitStruct.TIM_Period = 1000;
     timInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
     timInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
 
     TIM_TimeBaseInit(TIM1, &timInitStruct);
 
+    nvicInitTypeDef.NVIC_IRQChannelSubPriority = 0;
+    nvicInitTypeDef.NVIC_IRQChannel = TIM2_IRQn;
+    nvicInitTypeDef.NVIC_IRQChannelPreemptionPriority = 2;
+    nvicInitTypeDef.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&nvicInitTypeDef);
+
+    timInitStruct.TIM_Prescaler = 7200;
+    timInitStruct.TIM_Period = 1000;
+    timInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+    timInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInit(TIM2, &timInitStruct);
+
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+
+    TIM_Cmd(TIM2, ENABLE);
+
     TIM_OCInitTypeDef ocInitTypeDef;
     ocInitTypeDef.TIM_OCIdleState = TIM_OCIdleState_Set;
     ocInitTypeDef.TIM_OCNIdleState = TIM_OCNIdleState_Reset;
-    ocInitTypeDef.TIM_OCMode = TIM_OCMode_PWM2;
+    ocInitTypeDef.TIM_OCMode = TIM_OCMode_PWM1;
     ocInitTypeDef.TIM_OCPolarity = TIM_OCPolarity_High;
-    ocInitTypeDef.TIM_OCNPolarity = TIM_OCNPolarity_High;
+    ocInitTypeDef.TIM_OCNPolarity = TIM_OCNPolarity_Low;
     ocInitTypeDef.TIM_OutputState = TIM_OutputState_Enable;
     ocInitTypeDef.TIM_OutputNState = TIM_OutputNState_Enable;
-    ocInitTypeDef.TIM_Pulse = 50;
+    ocInitTypeDef.TIM_Pulse = 500;
 
     TIM_OC1Init(TIM1, &ocInitTypeDef);
-    TIM_OC2Init(TIM1, &ocInitTypeDef);
-    TIM_OC3Init(TIM1, &ocInitTypeDef);
-
     TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+    ocInitTypeDef.TIM_Pulse = 500;
+    TIM_OC2Init(TIM1, &ocInitTypeDef);
+    TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+    ocInitTypeDef.TIM_Pulse = 500;
+    TIM_OC3Init(TIM1, &ocInitTypeDef);
+    TIM_OC3PreloadConfig(TIM1, TIM_OCPreload_Enable);
 
     TIM_BDTRInitStructure.TIM_OSSRState = TIM_OSSRState_Enable;
     TIM_BDTRInitStructure.TIM_OSSIState = TIM_OSSIState_Enable;
     TIM_BDTRInitStructure.TIM_LOCKLevel = TIM_LOCKLevel_1;
     TIM_BDTRInitStructure.TIM_DeadTime = 0xFF;
     TIM_BDTRInitStructure.TIM_Break = TIM_Break_Disable;
-    TIM_BDTRInitStructure.TIM_BreakPolarity = TIM_BreakPolarity_High;
+    TIM_BDTRInitStructure.TIM_BreakPolarity = TIM_BreakPolarity_Low;
     TIM_BDTRInitStructure.TIM_AutomaticOutput = TIM_AutomaticOutput_Enable;
 
     TIM_BDTRConfig(TIM1, &TIM_BDTRInitStructure);
@@ -67,6 +91,5 @@ int main() {
     TIM_CtrlPWMOutputs(TIM1, ENABLE);
 
     while (1) {
-        GPIO_SetBits(GPIOC, GPIO_Pin_13);
     }
 }
---
+++ src/stm32f10x_it.c
@@ -0,0 +1,175 @@
+#include "stm32f10x.h"
+#include "stm32f10x_it.h"
+
+#define ACTIVATE_R1 TIM1->CCER |= (TIM_CCER_CC1E | TIM_CCER_CC1NE)
+#define ACTIVATE_R2 TIM1->CCER |= (TIM_CCER_CC2E | TIM_CCER_CC2NE)
+#define ACTIVATE_R3 TIM1->CCER |= (TIM_CCER_CC3E | TIM_CCER_CC3NE)
+#define DEACTIVATE_R1 TIM1->CCER &= ~(TIM_CCER_CC1E | TIM_CCER_CC1NE)
+#define DEACTIVATE_R2 TIM1->CCER &= ~(TIM_CCER_CC2E | TIM_CCER_CC2NE)
+#define DEACTIVATE_R3 TIM1->CCER &= ~(TIM_CCER_CC3E | TIM_CCER_CC3NE)
+
+/**
+  * @brief   This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void) {
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void) {
+    /* Go to infinite loop when Hard Fault exception occurs */
+    while (1) {
+    }
+}
+
+/**
+  * @brief  This function handles Memory Manage exception.
+  * @param  None
+  * @retval None
+  */
+void MemManage_Handler(void) {
+    /* Go to infinite loop when Memory Manage exception occurs */
+    while (1) {
+    }
+}
+
+/**
+  * @brief  This function handles Bus Fault exception.
+  * @param  None
+  * @retval None
+  */
+void BusFault_Handler(void) {
+    /* Go to infinite loop when Bus Fault exception occurs */
+    while (1) {
+    }
+}
+
+/**
+  * @brief  This function handles Usage Fault exception.
+  * @param  None
+  * @retval None
+  */
+void UsageFault_Handler(void) {
+    /* Go to infinite loop when Usage Fault exception occurs */
+    while (1) {
+    }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void) {
+}
+
+/**
+  * @brief  This function handles Debug Monitor exception.
+  * @param  None
+  * @retval None
+  */
+void DebugMon_Handler(void) {
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void) {
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void) {
+}
+
+/******************************************************************************/
+/*                 STM32F10x Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f10x_xx.s).                                            */
+/******************************************************************************/
+
+volatile uint8_t state = 0;
+
+void TIM2_IRQHandler(void) {
+    int16_t alpha_1, alpha_2, alpha_3;
+
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update)) {
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+
+        GPIOC->ODR ^= 0x2000;
+
+        switch (state) {
+            case 0:
+                state = 1;
+
+                DEACTIVATE_R2;
+                ACTIVATE_R3;
+
+                TIM1->CCR1 = 800;
+                TIM1->CCR3 = 200;
+                break;
+            case 1:
+                state = 2;
+
+                DEACTIVATE_R1;
+                ACTIVATE_R2;
+
+                TIM1->CCR2 = 800;
+                TIM1->CCR3 = 200;
+
+                break;
+            case 2:
+                state = 3;
+
+                ACTIVATE_R1;
+                DEACTIVATE_R3;
+
+                TIM1->CCR1 = 200;
+                TIM1->CCR2 = 800;
+
+                break;
+            case 3:
+                state = 4;
+
+                DEACTIVATE_R2;
+                ACTIVATE_R3;
+
+                TIM1->CCR1 = 200;
+                TIM1->CCR3 = 800;
+
+                break;
+            case 4:
+                state = 5;
+
+                ACTIVATE_R2;
+                DEACTIVATE_R1;
+
+                TIM1->CCR2 = 200;
+                TIM1->CCR3 = 800;
+
+                break;
+            case 5:
+                state = 0;
+
+                ACTIVATE_R1;
+                DEACTIVATE_R3;
+
+                TIM1->CCR1 = 800;
+                TIM1->CCR2 = 200;
+
+                break;
+        }
+    }
+}
+
